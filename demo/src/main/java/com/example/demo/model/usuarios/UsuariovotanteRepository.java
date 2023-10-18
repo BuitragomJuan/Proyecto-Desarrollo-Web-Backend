40d1575ac159e75753e07c692d16fd0b240e69a5
Juan Buitrago <buitragomjuan@outlook.com>
2023-10-18 18:17:55 -0500

    pruebas unitarias de las clases completas

--- demo/src/main/java/com/example/demo/model/usuarios/UsuariovotanteRepository.java
+++ demo/src/main/java/com/example/demo/model/usuarios/UsuariovotanteRepository.java
@@ -1,7 +1,9 @@
 package com.example.demo.model.usuarios;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsuariovotanteRepository extends CrudRepository<Usuariovotante,String>{
 
 }
