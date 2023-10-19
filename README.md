1109994cc4114135ca1f0f599a81c373bb0d9642
Frank5005 <frankill.5005@gmail.com>
2023-10-19 17:20:02 -0500

    Coma m

--- README.md
+++
@@ -1,2 +0,0 @@
-# Proyecto-Desarrollo-Web-Backend
-Componente backend del proyecto de desarrollo web 2023-03 Javeriana
--- demo/src/main/java/com/example/demo/model/usuarios/UsuarioAdmonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.model.usuarios;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsuarioAdmonRepository extends CrudRepository<UsuarioAdmon,String>{
-    
-}
